bfd964536f84e2a83c2068c9108f73a1f8d47447
Nanda Nurcholis <Nanda_N369@mitrais.com>
2020-08-29 17:19:22 +0700

    second init

--- authorization.js
+++ authorization.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = () => {
     return (req, res, next) => {
-        const token = req.headers("authorization");
+        const token = req.headers["authorization"];
         if(!token) {
             return res.status(401).send("access denied");
         } else {
